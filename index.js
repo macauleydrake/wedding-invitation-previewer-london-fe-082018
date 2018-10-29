f65b711b9d8f58315d71d75b4fbf4cf33a7e555d
Macauley Drake <macauleyann@gmail.com>
2018-10-29 20:24:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -125,6 +125,7 @@ document.addEventListener("DOMContentLoaded", function(event){
   document.getElementById("lastname").style.color = "#ffe8fd";
   document.getElementById("lastname").style.fontSize = "90px";
   document.getElementById("lastname").style.padding = "30px";
+  document.getElementById("lastname").style.font = "30px";
   
   
   document.getElementById("and").style.display = "block";
