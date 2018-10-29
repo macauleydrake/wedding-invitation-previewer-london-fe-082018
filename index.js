fe27fa59220016d029604476af5f53759d0def24
Macauley Drake <macauleyann@gmail.com>
2018-10-29 20:06:24 +0000

    Done.

--- index.js
+++ index.js
@@ -3,12 +3,86 @@ document.addEventListener("DOMContentLoaded", function(event){
   const floral = document.getElementById('floral')
    floral.addEventListener('click', function(event) {
   document.getElementById("background").style.backgroundImage = "url(https://images.unsplash.com/photo-1516617442634-75371039cb3a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6febeaaa69fd0cab717c6bab09776051&auto=format&fit=crop&w=2466&q=80)";
-  document.getElementById("firstname").style.fontFamily = "Impact,Charcoal,sans-serif"; 
-  document.getElementById("firstname").style.letterSpacing = "0px";
-  document.getElementById("firstname").style.color = "orange";
-  document.getElementById("secondname").style.fontFamily = "Impact,Charcoal,sans-serif";
-  document.getElementById("secondname").style.letterSpacing = "0px";
-  document.getElementById("secondname").style.color = "orange";
+  document.getElementById("invite").style.display = "block";
+  document.getElementById("invite").style.lineHeight = "normal"
+  document.getElementById("invite").style.textAlign = "center";
+  document.getElementById("invite").style.fontFamily = "'Pacifico', cursive";
+  document.getElementById("invite").style.letterSpacing = "2px";
+  document.getElementById("invite").style.fontSize = "14px";
+  document.getElementById("invite").style.padding = "10px"; 
+  document.getElementById("invite").style.color = "#f2fff6";
+  
+  document.getElementById("firstname").style.display = "block";
+  document.getElementById("firstname").style.lineHeight = "normal"
+  document.getElementById("firstname").style.textAlign = "center";
+  document.getElementById("firstname").style.fontFamily = "'Bad Script', cursive";
+  document.getElementById("firstname").style.letterSpacing = "10px";
+  document.getElementById("firstname").style.color = "#f9dee4";
+  document.getElementById("firstname").style.fontSize = "30px";
+  document.getElementById("firstname").style.padding = "0px";
+  
+  document.getElementById("secondname").style.display = "block";
+  document.getElementById("secondname").style.lineHeight = "normal"
+  document.getElementById("secondname").style.textAlign = "center";
+  document.getElementById("secondname").style.fontFamily = "'Bad Script', cursive";
+  document.getElementById("secondname").style.letterSpacing = "10px";
+  document.getElementById("secondname").style.color = "#f9dee4";
+  document.getElementById("secondname").style.fontSize = "30px";
+  document.getElementById("secondname").style.padding = "0px";
+  
+  document.getElementById("lastname").style.display = "block";
+  document.getElementById("lastname").style.lineHeight = "normal"
+  document.getElementById("lastname").style.textAlign = "center";
+  document.getElementById("lastname").style.display = "block";
+  document.getElementById("lastname").style.fontFamily = "'Sacramento', cursive";
+  document.getElementById("lastname").style.letterSpacing = "15px";
+  document.getElementById("lastname").style.color = "#f2fff6";
+  document.getElementById("lastname").style.fontSize = "80px";
+  document.getElementById("lastname").style.padding = "10px";
+  
+  document.getElementById("and").style.display = "block";
+  document.getElementById("and").style.lineHeight = "normal"
+  document.getElementById("and").style.textAlign = "center";
+  document.getElementById("and").style.padding = "0px";
+  document.getElementById("and").style.fontSize = "30px";
+  document.getElementById("and").style.fontFamily = "'Cookie', cursive";
+  document.getElementById("and").style.color = "#914b53";
+  
+  document.getElementById("on").style.display = "block";
+  document.getElementById("on").style.lineHeight = "normal"
+  document.getElementById("on").style.textAlign = "center";
+  document.getElementById("on").style.fontFamily = "'Pacifico', cursive";
+  document.getElementById("on").style.letterSpacing = "2px";
+  document.getElementById("on").style.fontSize = "15px";
+  document.getElementById("on").style.padding = "0px"; 
+  document.getElementById("on").style.color = "#914b53";
+  
+  document.getElementById("at").style.display = "block";
+  document.getElementById("at").style.lineHeight = "normal"
+  document.getElementById("at").style.textAlign = "center";
+  document.getElementById("at").style.fontFamily = "'Pacifico', cursive";
+  document.getElementById("at").style.letterSpacing = "0px";
+  document.getElementById("at").style.fontSize = "15px";
+  document.getElementById("at").style.padding = "0px"; 
+  document.getElementById("at").style.color = "#914b53";
+  
+  document.getElementById("date").style.display = "block";
+  document.getElementById("date").style.lineHeight = "normal"
+  document.getElementById("date").style.textAlign = "center";
+  document.getElementById("date").style.fontFamily = "'Sacramento', cursive";
+  document.getElementById("date").style.letterSpacing = "0px";
+  document.getElementById("date").style.color = "#f9dee4";
+  document.getElementById("date").style.fontSize = "30px";
+  document.getElementById("date").style.padding = "0px";
+  
+  document.getElementById("location").style.display = "block";
+  document.getElementById("location").style.lineHeight = "normal"
+  document.getElementById("location").style.textAlign = "center";
+  document.getElementById("location").style.fontFamily = "'Sacramento', cursive";
+  document.getElementById("location").style.letterSpacing = "0px";
+  document.getElementById("location").style.color = "#f9dee4";
+  document.getElementById("location").style.fontSize = "30px";
+  document.getElementById("location").style.padding = "0px";
    })
    
   
@@ -17,36 +91,158 @@ document.addEventListener("DOMContentLoaded", function(event){
     const magical = document.getElementById('magical')
    magical.addEventListener('click', function(event) {
   document.getElementById("background").style.backgroundImage = "url(https://images.unsplash.com/photo-1527190997915-67ce3b53cc58?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1b3bf88a842b778b89ca52af5c096649&auto=format&fit=crop&w=668&q=80)";
-  document.getElementById("firstname").style.fontFamily = "'Roboto Mono', monospace";
-  document.getElementById("firstname").style.letterSpacing = "5px";
-  document.getElementById("firstname").style.color = "red";
+  document.getElementById("invite").style.display = "block";
+  document.getElementById("invite").style.textAlign = "center";
+  document.getElementById("invite").style.fontFamily = "'Arvo', serif";
+  document.getElementById("invite").style.letterSpacing = "3px";
+  document.getElementById("invite").style.fontSize = "9px";
+  document.getElementById("invite").style.paddingTop = "15px"; 
+  document.getElementById("invite").style.color = "#9fb6c4";
   
-  document.getElementById("secondname").style.fontFamily = "'Roboto Mono', monospace";
-  document.getElementById("secondname").style.letterSpacing = "5px";
-  document.getElementById("secondname").style.color = "red";
+  document.getElementById("firstname").style.display = "block";
+  document.getElementById("firstname").style.textAlign = "left";
+  document.getElementById("firstname").style.fontFamily = "'Cutive Mono', monospace";
+  document.getElementById("firstname").style.letterSpacing = "10px";
+  document.getElementById("firstname").style.color = "#fcf0d9";
+  document.getElementById("firstname").style.fontSize = "40px";
+  document.getElementById("firstname").style.padding = "0px";
   
-  document.getElementById("lastname").style.fontFamily = "'Charmonman', cursive";
-  document.getElementById("lastname").style.fontSize = "50px";
-  document.getElementById("lastname").style.color = "pink";
-  document.getElementById("lastname").style.padding = "0px";
-  document.getElementById("lastname").style.margin = "20px";
+  document.getElementById("secondname").style.display = "block";
+  document.getElementById("secondname").style.textAlign = "left";
+  document.getElementById("secondname").style.fontFamily = "'Cutive Mono', monospace";
+  document.getElementById("secondname").style.letterSpacing = "10px";
+  document.getElementById("secondname").style.color = "#fcf0d9";
+  document.getElementById("secondname").style.fontSize = "40px";
+  document.getElementById("secondname").style.padding = "0px";
   
-  document.getElementById("date").style.fontFamily = "'Nanum Gothic', sans-serif";
+  document.getElementById("lastname").style.display = "block";
+  document.getElementById("lastname").style.display = "block";
+  document.getElementById("lastname").style.fontFamily = "'Josefin Slab', serif";
+  document.getElementById("lastname").style.letterSpacing = "18px";
+  document.getElementById("lastname").style.color = "#ffe8fd";
+  document.getElementById("lastname").style.fontSize = "90px";
+  document.getElementById("lastname").style.padding = "30px";
   
-  document.getElementById("location").style.fontFamily = "'Roboto Mono', monospace";
+  document.getElementById("and").style.display = "block";
+  document.getElementById("and").style.textAlign = "left";
+  document.getElementById("and").style.textAlign = "left";
+  document.getElementById("and").style.padding = "2px";
+  document.getElementById("and").style.fontSize = "9px";
+  document.getElementById("and").style.fontFamily = "'Arvo', serif";
+  document.getElementById("and").style.color = "#9fb6c4";
   
+  document.getElementById("on").style.display = "block";
+  document.getElementById("on").style.textAlign = "left";
+  document.getElementById("on").style.fontFamily = "'Arvo', serif";
+  document.getElementById("on").style.letterSpacing = "3px";
+  document.getElementById("on").style.fontSize = "9px";
+  document.getElementById("on").style.padding = "5px"; 
+  document.getElementById("on").style.color = "#9fb6c4";
+  
+  document.getElementById("at").style.display = "block";
+  document.getElementById("at").style.textAlign = "left";
+  document.getElementById("at").style.fontFamily = "'Arvo', serif";
+  document.getElementById("at").style.letterSpacing = "3px";
+  document.getElementById("at").style.fontSize = "9px";
+  document.getElementById("at").style.padding = "5px"; 
+  document.getElementById("at").style.color = "#9fb6c4";
+  
+  document.getElementById("date").style.display = "block";
+  document.getElementById("date").style.textAlign = "left";
+  document.getElementById("date").style.fontFamily = "'Josefin Slab', serif";
+  document.getElementById("date").style.letterSpacing = "10px";
+  document.getElementById("date").style.color = "#9fb6c4";
+  document.getElementById("date").style.fontSize = "20px";
+  document.getElementById("date").style.padding = "5px";
+  
+  document.getElementById("location").style.display = "block";
+  document.getElementById("location").style.textAlign = "left";
+  document.getElementById("location").style.fontFamily = "'Josefin Slab', serif";
+  document.getElementById("location").style.letterSpacing = "10px";
+  document.getElementById("location").style.color = "#9fb6c4";
+  document.getElementById("location").style.fontSize = "20px";
+  document.getElementById("location").style.padding = "5px";
    })
    
    
     const cats = document.getElementById('cats')
    cats.addEventListener('click', function(event) {
   document.getElementById("background").style.backgroundImage = "url(https://images.unsplash.com/photo-1516139008210-96e45dccd83b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ac07676efc2ca5b11b548d8ae01301ba&auto=format&fit=crop&w=800&q=60)";
-  document.getElementById("firstname").style.fontFamily = "cursive";
-  document.getElementById("firstname").style.letterSpacing = "0px";
-  document.getElementById("firstname").style.color = "green";
-  document.getElementById("secondname").style.fontFamily = "cursive";
-  document.getElementById("secondname").style.letterSpacing = "0px";
-  document.getElementById("secondname").style.color = "green";
+   document.getElementById("invite").style.display = "block";
+  document.getElementById("invite").style.textAlign = "center";
+  document.getElementById("invite").style.fontFamily = "'Amatic SC', cursive, regular";
+  document.getElementById("invite").style.letterSpacing = "2px";
+  document.getElementById("invite").style.fontSize = "14px";
+  document.getElementById("invite").style.paddingTop = "0px"; 
+  document.getElementById("invite").style.color = "#20302a";
+  
+  document.getElementById("firstname").style.display = "block";
+  document.getElementById("firstname").style.textAlign = "center";
+  document.getElementById("firstname").style.fontFamily = "'Permanent Marker', cursive";
+  document.getElementById("firstname").style.letterSpacing = "5px";
+  document.getElementById("firstname").style.lineHeight = "10px";
+  document.getElementById("firstname").style.color = "#416055";
+  document.getElementById("firstname").style.fontSize = "30px";
+  document.getElementById("firstname").style.padding = "0px";
+  
+  document.getElementById("secondname").style.display = "block";
+  document.getElementById("secondname").style.textAlign = "center";
+  document.getElementById("secondname").style.fontFamily = "'Permanent Marker', cursive";
+  document.getElementById("secondname").style.letterSpacing = "5px";
+  document.getElementById("secondname").style.lineHeight = "10px"
+  document.getElementById("secondname").style.color = "#416055";
+  document.getElementById("secondname").style.fontSize = "30px";
+  document.getElementById("secondname").style.padding = "0px";
+  
+  document.getElementById("lastname").style.display = "block";
+  document.getElementById("lastname").style.textAlign = "center";
+  document.getElementById("lastname").style.display = "block";
+  document.getElementById("lastname").style.fontFamily = "'Permanent Marker', cursive";
+  document.getElementById("lastname").style.letterSpacing = "40px";
+  document.getElementById("lastname").style.lineHeight = "90px"
+  document.getElementById("lastname").style.color = "#2d2d29";
+  document.getElementById("lastname").style.fontSize = "60px";
+  document.getElementById("lastname").style.padding = "0px";
+  
+  document.getElementById("and").style.display = "block";
+  document.getElementById("and").style.textAlign = "center";
+  document.getElementById("and").style.padding = "0px";
+  document.getElementById("and").style.fontSize = "20px";
+  document.getElementById("and").style.lineHeight = "50px"
+  document.getElementById("and").style.fontFamily = "'Amatic SC', cursive, regular";
+  document.getElementById("and").style.color = "#20302a";
+  
+  document.getElementById("on").style.display = "block";
+  document.getElementById("on").style.textAlign = "center";
+  document.getElementById("on").style.fontFamily = "'Amatic SC', cursive, regular";
+  document.getElementById("on").style.letterSpacing = "2px";
+  document.getElementById("on").style.fontSize = "15px";
+  document.getElementById("on").style.padding = "0px"; 
+  document.getElementById("on").style.color = "#2d2d29";
+  
+  document.getElementById("at").style.display = "block";
+  document.getElementById("at").style.textAlign = "center";
+  document.getElementById("at").style.fontFamily = "'Amatic SC', cursive, regular";
+  document.getElementById("at").style.letterSpacing = "2px";
+  document.getElementById("at").style.fontSize = "15px";
+  document.getElementById("at").style.padding = "0px"; 
+  document.getElementById("at").style.color = "#2d2d29";
+  
+  document.getElementById("date").style.display = "block";
+  document.getElementById("date").style.textAlign = "center";
+  document.getElementById("date").style.fontFamily = "'Amatic SC', cursive, regular";
+  document.getElementById("date").style.letterSpacing = "0px";
+  document.getElementById("date").style.color = "#416055";
+  document.getElementById("date").style.fontSize = "30px";
+  document.getElementById("date").style.padding = "5px";
+  
+  document.getElementById("location").style.display = "block";
+  document.getElementById("location").style.textAlign = "center";
+  document.getElementById("location").style.fontFamily = "'Amatic SC', cursive, regular";
+  document.getElementById("location").style.letterSpacing = "0px";
+  document.getElementById("location").style.color = "#416055";
+  document.getElementById("location").style.fontSize = "30px";
+  document.getElementById("location").style.padding = "5px";
    })
 
 const go = document.getElementById('go')
